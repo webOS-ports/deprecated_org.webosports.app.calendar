aee115693700b141e9e507c778d1e17ed7e43194
Jordan Gensler <jordangens@gmail.com>
2013-01-30 15:40:39 -0600

    Cordova Testing

--- source/App.js
+++ source/App.js
@@ -25,8 +25,10 @@ enyo.kind({
 	],
 
 	doneFirstUse: function(){
+		navigator.notification.beep(1);
+		//TODO: Abstract the cordova function to our own function that does the same thing, just prettier.
 		navigator.notification.alert(
-		    'You are the winner!',  // message
+		    'Testing',  // message
 		    function(){},         // callback
 		    'Game Over',            // title
 		    'Done'                  // buttonName
